316ec3cbd854ea53967acc3e0a6c21fdf1625f09
pktubs <p.khera2004@gmail.com>
2024-02-22 18:45:20 -0500

    adding middleware

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
-const express = require('express')
+require('dotenv').config()
 
+const express = require('express')
 const app = express()
+
+app.use((req, res, next)=> {
+    console.log(req.path, req.method)
+    next()
+})
+
 
 app.get('/', (req, res) => {res.json({mssg:'Welcome to the app'})})
 
-app.listen(4000, () => {
-    console.log('listening on port 4000!')
+app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT)
 })
+
