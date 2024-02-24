04367d3d90e48d5d8e4c314c57580b284be46cf0
pktubs <p.khera2004@gmail.com>
2024-02-24 13:46:45 -0500

    Schema creations and creating a workout document

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const Workout = require('../models/workout')
 const router = express.Router()
 
 router.get('/', (req,res)=>{
@@ -10,11 +10,22 @@ res.json({mssg: 'GET ALL WORKOUTS'})
 router.get('/:id', (req, res)=>{
     res.json({mssg: 'Get a single workout'})
 })
-router.post('/', (req, res)=>{
+router.post('/', async (req, res)=>{
+    const {title, load, reps} = req.body
+
+    try{
+        const workout = await Workout. create({title, load, reps})
+        res.status(200).json(workout)
+
+    } catch(error){
+        res.status(404).json({error: error.message})
+
+    }
     res.json({mssg: 'Post a new workout'})
 })
 
 router.delete('/:id', (req, res)=>{
+    
     res.json({mssg: 'delete a workout'})
 })
 
