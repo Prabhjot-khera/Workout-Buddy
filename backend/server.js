f3c05ada83cc0b08e1a2718c5e90d4429b358454
pktubs <p.khera2004@gmail.com>
2024-02-24 11:03:43 -0500

    mongodb connection via mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 
 const workoutroutes = require('./routes/workouts')
 const app = express()
@@ -15,7 +16,17 @@ app.use((req, res, next)=> {
 
 app.use('/api/workouts',workoutroutes)
 
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
-})
+//connect to db
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=> {
+        app.listen(process.env.PORT, () => {
+            console.log('listening on port', process.env.PORT)
+        })
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+
+
 
