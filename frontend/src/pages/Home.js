8f4148efd4ad243ed39f50e88431b829c5839350
pktubs <p.khera2004@gmail.com>
2024-02-28 16:30:54 -0500

    adding new workout form

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState} from 'react'
 import WorkoutDetails from '../components/WorkoutDetails'
+import WorkoutForm from '../components/WorkoutForm'
 
 const Home = () => {
     const [workouts, setWorkouts] = useState(null)
@@ -27,6 +28,8 @@ const Home = () => {
 
                 )) }
             </div>
+
+            <WorkoutForm />
 
             
         </div>
